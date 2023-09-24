918bb2f5e1fff9d409b3b49dac23ee1074bba734
Calcoph <34724728+Calcoph@users.noreply.github.com>
2023-09-24 22:51:17 +0200

    test script complete

--- py_test/test.py
+++ py_test/test.py
@@ -1,5 +1,36 @@
-from rs_hexpyt import sum_as_string, translate_file
+from rs_hexpyt import translate_file
 
-print(translate_file.__text_signature__)
-print(translate_file("a", "b"))
-print(sum_as_string(5, 6))
+import os
+
+class Enum:
+    pass
+
+class Environment(Enum):
+    INNER = 0
+    OUTER = 1
+
+try:
+    pattern_inputs = os.listdir("pattern_inputs")
+    env = Environment.INNER
+except:
+    pattern_inputs = os.listdir("py_test/pattern_inputs")
+    env = Environment.OUTER
+
+if env == Environment.INNER:
+    in_path = lambda x: f"pattern_inputs/{x}"
+    out_path = lambda x: f"pattern_outputs/{x}"
+    out_dir = "pattern_outputs"
+if env == Environment.OUTER:
+    in_path = lambda x: f"py_test/pattern_inputs/{x}"
+    out_path = lambda x: f"py_test/pattern_outputs/{x}"
+    out_dir = "py_test/pattern_outputs"
+
+if not os.path.exists(out_dir):
+    if env == Environment.INNER:
+        os.mkdir("pattern_outputs")
+    if env == Environment.OUTER:
+        os.mkdir("py_test/pattern_outputs")
+
+for pattern_input in pattern_inputs:
+    pattern_output = pattern_input.split(".")[0] + ".py"
+    translate_file(in_path(pattern_input), out_path(pattern_output))
